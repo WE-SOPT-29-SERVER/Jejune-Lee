0263e1c4f5ac3c8ce83a9cf6517615652a9dcb5e
LeeJejune <leejejune0625@gmail.com>
2021-11-25 20:11:18 +0900

    [add] 2차 세미나 과제
    [add] 2차 세미나 과제

---
+++ 2st-seminar/cal.js
@@ -0,0 +1,11 @@
+const { divide, add, multiply, substract } = require("./sum");
+
+const result = add(1,3);
+const result1 = divide(1,3);
+const result2 = multiply(1,3);
+const result3 = substract(1,3);
+
+console.log("result = ", result);
+console.log("result1 = ", result1);
+console.log("result2 = ", result2);
+console.log("result3 = ", result3);
